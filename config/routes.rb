630e5b390ad9a80f9430f4f8a4155ef7c72924df
Chawakorn Suphepre <chawakorn.su@ku.th>
2017-05-08 05:29:42 +0000

    finish login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+root 'static_pages#home'
 
-  get 'static_pages/help'
+get '/help', to: 'static_pages#help'
 
-root 'application#hello'
+get '/about', to: 'static_pages#about'
 
+get '/contact', to: 'static_pages#contact'
+
+get '/home', to: 'static_pages#home'
+
+get '/login', to: 'static_pages#login'
 end
